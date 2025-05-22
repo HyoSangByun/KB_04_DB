56c314028129549f1cc574e1de8f4e3e57dbc2db
HyoSangByun <billy04100410@gmail.com>
2025-05-22 13:42:09 +0900

    20250522 오후 수업

--- travelapp/src/main/java/org/scoula/travel/domain/TravelVO.java
+++ travelapp/src/main/java/org/scoula/travel/domain/TravelVO.java
@@ -5,6 +5,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @Builder
 @NoArgsConstructor
@@ -16,4 +18,6 @@ public class TravelVO {
     private String description;
     private String address;
     private String phone;
+
+    private List<TravelImageVO> images;
 }
